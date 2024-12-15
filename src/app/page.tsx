95083d74d52620670157d42b3cc0a784825c42af
Gboly <gbolahanahmed1@gmail.com>
2024-12-15 14:15:05 +0100

    refact: Create dedicated file per section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
-import { Roboto, Roboto_Flex } from 'next/font/google';
 import React from 'react'
 import "./page.css"
-import Image from 'next/image';
+import Intro from '@/sections/home/intro/Intro';
 
-export const robotoFlex = Roboto_Flex({
-  subsets: ["latin"],
-  weight: "200",
-});
-export const roboto = Roboto({
-  subsets: ["latin"],
-  weight: "400",
-});
-
-const intro: string = `Harness the power of a global community to accelerate your project's success. Whether you’re raising funds for innovation, social impact, or creative ventures, by simply creating a campaign, you can connect with passionate supporters worldwide. All campaigns run on the fast, secure, and scalable Solana blockchain—ensuring transparency, trust, and efficiency every step of the way.`
 
 
 const Home = () => {
   return (
     <main className='home'>
-      <section className='intro'>
-        <p className={robotoFlex.className}>Fuel Dreams, Forge Impact</p>
-        <Image src={"/communityLight.png"} alt='Three rows of user emojis' width={1640} height={924} className='for-light'/>
-        <Image src={"/communityDark.png"} alt='Three rows of user emojis' width={1640} height={924} className='for-dark'/>
-        <p className={roboto.className}>{intro}</p>
-        <button>Create a campaign</button>
-      </section>
-
-      <section className='trending'>
-
-      </section>
+      <Intro />
+      
     </main>
   )
 }
---
+++ src/sections/home/intro/Intro.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./intro.css"
+import Image from 'next/image';
+import { roboto, robotoFlex } from '@/app/fonts';
+
+
+const intro: string = `Harness the power of a global community to accelerate your project's success. Whether you’re raising funds for innovation, social impact, or creative ventures, by simply creating a campaign, you can connect with passionate supporters worldwide. All campaigns run on the fast, secure, and scalable Solana blockchain—ensuring transparency, trust, and efficiency every step of the way.`
+
+const Intro = () => {
+  return (
+    <section className='intro'>
+    <p className={robotoFlex.className}>Fuel Dreams, Forge Impact</p>
+    <Image src={"/communityLight.png"} alt='Three rows of user emojis' width={1640} height={924} className='for-light'/>
+    <Image src={"/communityDark.png"} alt='Three rows of user emojis' width={1640} height={924} className='for-dark'/>
+    <p className={roboto.className}>{intro}</p>
+    <button>Create a campaign</button>
+  </section>
+  )
+}
+
+export default Intro
