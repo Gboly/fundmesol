dd390f63555b1beb0813b32b9580f821e29cdaa1
Gboly <gbolahanahmed1@gmail.com>
2024-12-15 14:25:25 +0100

    fix: Update global font

--- src/sections/home/intro/Intro.tsx
+++ src/sections/home/intro/Intro.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./intro.css"
 import Image from 'next/image';
-import { roboto, robotoFlex } from '@/app/fonts';
+import { robotoFlex } from '@/app/fonts';
 
 
 const intro: string = `Harness the power of a global community to accelerate your project's success. Whether you’re raising funds for innovation, social impact, or creative ventures, by simply creating a campaign, you can connect with passionate supporters worldwide. All campaigns run on the fast, secure, and scalable Solana blockchain—ensuring transparency, trust, and efficiency every step of the way.`
@@ -12,7 +12,7 @@ const Intro = () => {
     <p className={robotoFlex.className}>Fuel Dreams, Forge Impact</p>
     <Image src={"/communityLight.png"} alt='Three rows of user emojis' width={1640} height={924} className='for-light'/>
     <Image src={"/communityDark.png"} alt='Three rows of user emojis' width={1640} height={924} className='for-dark'/>
-    <p className={roboto.className}>{intro}</p>
+    <p>{intro}</p>
     <button>Create a campaign</button>
   </section>
   )
