e4776dfeffe6c2dc7f72fe9d7befd861e0fab9eb
Gboly <gbolahanahmed1@gmail.com>
2024-12-16 23:07:42 +0100

    fix: Create custom icon component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,13 +1,14 @@
 import { SvgIconTypeMap } from '@mui/material';
 import { OverridableComponent } from '@mui/material/OverridableComponent';
 import React from 'react'
+import "./icon.css"
 
 interface props {
     icon: OverridableComponent<SvgIconTypeMap<object, "svg">> & {muiName: string};
 }
 const Icon = ({icon: Icon}: props) => {
   return (
-    <i>
+    <i className='icon'>
         <Icon style={{ color: "inherit", fontSize: "inherit" }} />
     </i>
   )
