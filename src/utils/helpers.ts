979b271ab3539387cfef59c30f93c7499fcdf1ca
Gboly <gbolahanahmed1@gmail.com>
2024-12-23 09:30:46 +0100

    fix: Modify the way of checking current theme

---
+++ src/utils/helpers.ts
@@ -0,0 +1,10 @@
+// const theme: string | undefined | null = document.querySelector("html")?.getAttribute("data-theme")
+// export const isDarkMode: boolean = theme === "dark"
+// export const isLightMode: boolean = theme === "light"
+// console.log(theme);
+
+// This file loads before the html element mounts so this would be a fxn just so that the theme is fetched at the point of need where the the mounting must have been done.
+type getAttributeFxn = () => string | undefined | null
+export const getTheme: getAttributeFxn = () => document.querySelector("html")?.getAttribute("data-theme")
+export const ifDarkMode: () => boolean = () => getTheme() === "dark"
+export const ifLightMode: () => boolean = () => getTheme() === "light"
