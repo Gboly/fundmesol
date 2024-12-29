ea231c684c7826594bc822d3cedee4de12f69225
Gboly <gbolahanahmed1@gmail.com>
2024-12-29 14:18:27 +0100

    fix: Unlock more font weights

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,12 +2,12 @@ import { Economica, Roboto, Roboto_Flex } from 'next/font/google';
 
 export const robotoFlex = Roboto_Flex({
     subsets: ["latin"],
-    weight: ["200", "600"]
+    weight: ["200", "400", "600"]
 });
 
 export const roboto = Roboto({
     subsets: ["latin"],
-    weight: "400",
+    weight: ["400", "500", "700"],
 });
 
 export const economica = Economica({
