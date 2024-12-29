70d1864e34809c4b7590aef319779cf6bd4e78a4
Gboly <gbolahanahmed1@gmail.com>
2024-12-29 14:50:50 +0100

    feat: Create Table to show campaign funding history

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled, TableHead } from '@mui/material';
+import { TablePaginationActions } from './Pagination';
+import { fundingHistoryRows as rows } from '@/utils/dummydata';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+}));
+
+interface headerType {id: number, title: string, label: string}
+const rowsPerPage = 5
+const header: headerType[] = [
+    {id: 1, title: "Wallet Address", label: "address"},
+    {id: 2, title: "Amount (sol)", label: "amount"},
+    {id: 3, title: "% Contribution", label: "percentage"},
+]
+export default function CustomPaginationActionsTable() {
+  const [page, setPage] = React.useState(0);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableHead>
+          <TableRow>
+            {header.map(({title, id}) => <StyledTableCell key={title} align={id > 1 ? "right" : "left"}>{title}</StyledTableCell>)}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
+            <StyledTableRow key={row.address}>
+              <StyledTableCell component="th" scope="row">
+                {row.address}
+              </StyledTableCell>
+              <StyledTableCell style={{ width: 160 }} align="right">
+                {row.amount}
+              </StyledTableCell>
+              <StyledTableCell style={{ width: 160 }} align="right">
+                {row.percentage}
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+          {emptyRows > 0 && (
+            <StyledTableRow style={{ height: 53 * emptyRows }}>
+              <StyledTableCell colSpan={6} />
+            </StyledTableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  inputProps: {
+                    'aria-label': 'rows per page',
+                  },
+                  native: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/utils/dummydata.ts
+++ src/utils/dummydata.ts
@@ -58,3 +58,26 @@ export const campaignsData: campaignCardsDetails = [
         deadline: "5hrs"
     },
 ]
+
+function createData(address: string, amount: number, percentage: number) {
+    return { address, amount, percentage };
+}
+
+export const fundingHistoryRows = [
+    createData('DfiHPh4VyKSTVF6eqhBfbxpiZrEkrHXt2ZE9JS5qHhBU', 3.8, 19),
+    createData('8zJ5xX7PEkYHgwkUg5xzwENxm4hkE9drBsCvgzE9KHGt', 2.6, 13),
+    createData('4NwS4ezQ3tU2UzuwXKVzHj8YxQyJvE8YaGqBaX6dNcBf', 2.2, 11),
+    createData('GWvhqdYfNFqTuqh4gXL91CSdDKJGYLzs8sHzRi9qF4PZ', 1.8, 9),
+    createData('Hx6LbkMrW3H5NVaZjvbJRqiHrcvVi7uWCqwWZSKkgBXD', 1.6, 8),
+    createData('9ZNTfG4NyQgxy2SWjSPQGzUicdE5conGZXzgJXzDQB7K', 1.4, 7),
+    createData('EWmw9RKUkd5qxYKFRPNBAz4SzX6mDqG4QYkGH3wNedKK', 1.2, 6),
+    createData('3Gj5CXz8iz3gs9dKhSwDBvCmoGcCGZrXrtTKFHXyuWwF', 1.1, 5.5),
+    createData('7kqHBqxJqCE1h4oWvLvWFNFa5jkPpuz1HGvS5PAHqhL9', 0.9, 4.5),
+    createData('BXZ22xbckqLwqhw8pxGyy8ZCvWE4SpvyHgt7S6vEDUT5', 0.8, 4),
+    createData('5KtxEqMz6JxWgAL3qB6H2vHQGQkZkYt7FEJjXRJZfyTG', 0.7, 3.5),
+    createData('HNxVbMaMeKhCiYZPqLyc8dxGRWPrGX5RbCDZQo7vJhkS', 0.6, 3),
+    createData('4eJ3PTDcQvxJKvKzMyvJFRwSMYnJJHRhh7qMrG6EbBEn', 0.5, 2.5),
+    createData('6YxzRxZqp3L6BzDjX9bf9h3EHXkzwWjywvQ5KHtJ5QYq', 0.4, 2),
+    createData('2ZqaTLm4NxNkZJ36GEUZCkqRCD9ZDqGmka8HgBDsdVkU', 0.4, 2),
+    createData('9ZNTfG4NyQgxy2SWjSPQGzUicdE5conGZXzgJXzDQB7K', 1.4, 7)
+].sort((a, b) => (a.amount < b.amount ? -1 : 1));
